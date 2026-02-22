23c1a62fdc268a0f224d46ecec675f8550991225
jacobrubin99 <jacobrubin99@gmail.com>
2026-02-22 12:58:39 -0600

    update title mobile button

--- chewbies-eggciting-mystery/src/components/TitleScreen/TitleScreen.js
+++ chewbies-eggciting-mystery/src/components/TitleScreen/TitleScreen.js
@@ -2,6 +2,7 @@ import "./TitleScreen.css";
 import mysteryLogo from "../../assets/mystery game logo.png";
 import mysteryLogoChewbie from "../../assets/mystery game logo chewbie.png";
 import titleButton from "../../assets/buttons/hop-in-button.png";
+import titleMobileButton from "../../assets/buttons/hop-in-mobile-button.png";
 import titleChar from "../../assets/base/title page chewbie.png";
 
 const TitleScreen = ({ onNext }) => {
@@ -27,6 +28,12 @@ const TitleScreen = ({ onNext }) => {
           className="title-button"
           onClick={onNext}
           src={titleButton}
+          alt="Hop in!"
+        />
+        <img
+          className="title-button-mobile"
+          onClick={onNext}
+          src={titleMobileButton}
           alt="Hop in!"
         />
       </div>
