cf69e4123db263ff3f0c1807064f6c3590e18634
jacobrubin99 <jacobrubin99@gmail.com>
2026-02-22 12:53:59 -0600

    update title screen

--- chewbies-eggciting-mystery/src/components/TitleScreen/TitleScreen.js
+++ chewbies-eggciting-mystery/src/components/TitleScreen/TitleScreen.js
@@ -1,7 +1,7 @@
 import "./TitleScreen.css";
 import mysteryLogo from "../../assets/mystery game logo.png";
 import mysteryLogoChewbie from "../../assets/mystery game logo chewbie.png";
-import titleButton from "../../assets/buttons/title_button.png";
+import titleButton from "../../assets/buttons/hop-in-button.png";
 import titleChar from "../../assets/base/title page chewbie.png";
 
 const TitleScreen = ({ onNext }) => {
@@ -10,24 +10,24 @@ const TitleScreen = ({ onNext }) => {
       {/* <img
           className="mystery-char"
           src={titleChar}
-          alt="Mystery Game Chewbie Mascot"
+          alt="Easter Game Chewbie Mascot"
         /> */}
       <div className="content-container">
         {/* <img
           className="mystery-logo"
           src={mysteryLogo}
-          alt="Mystery Game Logo"
+          alt="Easter Game Logo"
         /> */}
-        <img
+        {/* <img
           className="mystery-logo-chewbie"
           src={mysteryLogoChewbie}
-          alt="Mystery Game Logo"
-        />
+          alt="Easter Game Logo"
+        /> */}
         <img
           className="title-button"
           onClick={onNext}
           src={titleButton}
-          alt="Let's Play"
+          alt="Hop in!"
         />
       </div>
     </div>
