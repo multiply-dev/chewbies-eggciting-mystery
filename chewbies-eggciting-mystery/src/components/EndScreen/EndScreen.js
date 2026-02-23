47f41b12108959eee69e700011031df3d205a19f
jacobrubin99 <jacobrubin99@gmail.com>
2026-02-22 18:00:22 -0600

    update end screen

--- chewbies-eggciting-mystery/src/components/EndScreen/EndScreen.js
+++ chewbies-eggciting-mystery/src/components/EndScreen/EndScreen.js
@@ -4,10 +4,7 @@ import "./EndScreen.css";
 const EndScreen = ({ onReplay }) => {
   return (
     <div className="end-screen">
-      <div className="smoke" />
-      <div className="chewbie" />
       <div className="text-container">
-        <h1 className="submit-text">Your guess has been submitted!</h1>
         {/* <img src={goodluck} alt="Good Luck" className="good-luck" /> */}
       </div>
     </div>
